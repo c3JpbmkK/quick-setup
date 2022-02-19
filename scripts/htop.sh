d0767950edf44b52a03ecd237cd1d86b0cfa4e46
Srinivasan Mohana Arunachalam <srinivasan.ma2009@hotmail.com>
2022-02-19 16:37:33 +0100

    Install htop from sources

---
+++ scripts/htop.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+RELEASES="https://github.com/htop-dev/htop/releases"
+
+VERSION="3.1.2"
+DOWNLOAD_URL="https://github.com/htop-dev/htop/releases/download/${VERSION}/htop-${VERSION}.tar.xz"
+CHECKSUM_URL="https://github.com/htop-dev/htop/releases/download/${VERSION}/htop-${VERSION}.tar.xz.sha256"
+
+DOWNLOAD_DIR="/tmp/htop-${VERSION}"
+mkdir "$DOWNLOAD_DIR" || echo "Error creating folder $DOWNLOAD_DIR"
+cd "$DOWNLOAD_DIR"
+
+curl -sSL -O "$DOWNLOAD_URL"
+curl -sSL -O "$CHECKSUM_URL"
+
+sha256sum -c htop-${VERSION}.tar.xz.sha256
+if [ $? -ne 0 ]
+then
+	echo "File integrity check failed. Cleaning up"
+	rm -rf "$DOWNLOAD_DIR" || echo "Error deleting forlder $DOWNLOAD_DIR"
+	exit 1
+fi
+
+tar -xvf htop-${VERSION}.tar.xz
+cd htop-${VERSION}
+./configure
+make && make install
+rm -rf "$DOWNLOAD_DIR" || echo "Error deleting forlder $DOWNLOAD_DIR"
+
